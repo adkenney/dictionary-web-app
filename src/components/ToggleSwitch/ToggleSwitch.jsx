0fe30d0fbe95aba0c64bec227c5db730b6e06803
Arkell Kenney <kell@Arkells-MacBook-Air.local>
2023-02-14 22:04:12 -0500

    Add toggle switch to header

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from '../ToggleSwitch/ToggleSwitch.module.css';
 
-const ToggleSwitch = () => {
-  return <div></div>;
+const ToggleSwitch = ({ toggle, handleToggle }) => {
+  const theme = toggle ? 'light' : 'dark';
+  return (
+    <div className={styles['toggle-container']} onClick={handleToggle}>
+      <div className={`${styles[theme]} ${styles['toggle-switch']}`}></div>
+    </div>
+  );
 };
 
 export default ToggleSwitch;
